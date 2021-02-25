d07a40e479988208b142327cbf59813e092ad615
Florian DOUSSIN <florian.doussin@groupeseloger.com>
2021-02-25 16:54:11 +0100

    Update project to swift 5 + add icons

---
+++ SwiftToolbox/SwiftToolbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftToolbox
+//
+//  Created by Florian DOUSSIN on 25/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
